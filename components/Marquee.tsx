f384471eaaa1199ec7abf50a412a5036b3058dce
Surjendu kar <rahulgotp9988@gmai.com>
2025-07-15 17:37:59 +0530

    Update Marquee component to adjust text line height and padding for improved visual presentation.

--- components/Marquee.tsx
+++ components/Marquee.tsx
@@ -2,8 +2,8 @@ function Marquee() {
   return (
     <div className="w-full py-10 bg-[#004D43] rounded-tl-3xl rounded-tr-3xl overflow-hidden">
       <div className="uppercase text border-t-2 border-b-2  border-zinc-300 font-grotesk flex gap-10  whitespace-nowrap">
-        <h1 className="text-[433px] leading-none">we are ochi</h1>
-        <h1 className="text-[433px] leading-none">we are ochi</h1>
+        <h1 className="text-[433px]  leading-[300px] pb-10">we are ochi</h1>
+        <h1 className="text-[433px]  leading-[300px] pb-10">we are ochi</h1>
       </div>
     </div>
   );
