9a1aa5daee1957107df1b0e9e49f61394a0dacce
Surjendu kar <rahulgotp9988@gmai.com>
2025-07-15 17:30:00 +0530

    Update LandingPage component to include overflow handling for text elements, enhancing layout and visual presentation.

---
+++ components/Marquee.tsx
@@ -0,0 +1,12 @@
+function Marquee() {
+  return (
+    <div className="w-full py-10 bg-[#004D43] rounded-tl-3xl rounded-tr-3xl overflow-hidden">
+      <div className="uppercase text border-t-2 border-b-2  border-zinc-300 font-grotesk flex gap-10  whitespace-nowrap">
+        <h1 className="text-[433px] leading-none">we are ochi</h1>
+        <h1 className="text-[433px] leading-none">we are ochi</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Marquee;
